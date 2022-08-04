757c6fb6e9b42a9e5b7f812cbfbf9f5f9f29ccbf
Marcelo <marceloggarciajr@gmail.com>
2022-08-03 23:31:17 -0300

    Adicionado arquivo + seleção de palavra

--- forca.py
+++ forca.py
@@ -1,9 +1,21 @@
 def jogar():
+    import random as rd
     print("*************")
     print("Jogo da Forca")
     print("*************")
 
-    palavra_secreta = "casas".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = rd.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
     numero_letras = (len(palavra_secreta))
     erros = 0
